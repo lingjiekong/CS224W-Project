6b7c1d70eded6437adb1f533e26bfdffffec2f77
Lingjie Kong <lingjik@amazon.com>
2021-03-20 01:09:33 -0700

    add pan_benchmark_ogb_loader_add_edge_weight_to_node.py

--- launch.sh
+++ launch.sh
@@ -1,1 +1,1 @@
-python pan_benchmark_ogb_loader_with_edge_feature.py --runs 10 --epochs 50 --batch_size 32 --learning_rate 0.001
+python pan_benchmark_ogb_loader_add_edge_weight_to_node.py --runs 5 --epochs 120 --batch_size 32 --learning_rate 0.00025
---
+++ mat.py
@@ -0,0 +1,4 @@
+import scipy.io as sio
+
+mat_contents = sio.loadmat("lr_0.0005_bz_32.mat")
+print(mat_contents)
